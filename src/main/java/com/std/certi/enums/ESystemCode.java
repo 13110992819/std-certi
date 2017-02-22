d4f973557d973d22a2d6f2a434b37c5e0548e68e
myb858 <myb858@hotmail.com>
2017-02-22 21:35:31 +0800

    update

--- src/main/java/com/std/certi/enums/ESystemCode.java
+++ src/main/java/com/std/certi/enums/ESystemCode.java
@@ -1,9 +1,9 @@
 package com.std.certi.enums;
 
-public enum ESystemId {
-    P2P("1", "P2P"), CPZC("2", "产品众筹"), ZHPAY("3", "正汇钱包");
+public enum ESystemCode {
+    GJS("1", "个金所"), ZHPAY("2", "正汇钱包");
 
-    ESystemId(String code, String value) {
+    ESystemCode(String code, String value) {
         this.code = code;
         this.value = value;
     }
